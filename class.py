2dc83b3b50b576857e55ed501c3a2c6eb12c0b34
oshmetochki <toni.krivorot@gmail.com>
2023-03-17 02:05:55 +0300

    class

---
+++ class.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+class cub:
+    def __unit__(self, al, high, long):
+        self.al=al
+        self.high=high
+        self.long=long
+@dataclasses
+class square:
+    dlina=int
+    shirina=int
